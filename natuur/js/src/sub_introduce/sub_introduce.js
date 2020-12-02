6f95db7f993b1ad1434ac062c27f0c44dc551e36
wjdgotn001 <wjdgotn001@gmail.com>
2020-12-02 13:02:34 +0900

    intro_animation

--- natuur/js/src/sub_introduce/sub_introduce.js
+++ natuur/js/src/sub_introduce/sub_introduce.js
@@ -23,32 +23,36 @@
     //일정 스크롤 시에 이미지 나타나게 하기
     var tabContents = $('.tab_contents');
     var tabConImg = tabContents.find('img');
+    var story = tabContents.find('.story_brand');
+    var storyImg = story.children('img');
     var storyP = tabContents.find('p');
-    // var storyCapi = tabContents.find('capitalize');
     var mainBg = tabContents.find('.main_bg');
+    var innerBg = $('.inner_bg_img');
+    var innerBgOffset = innerBg.offset().top;
+    var mainBgOffset = mainBg.offset().top;
     var win = $(window);
-    var it = $(this);
+    var winH = win.outerHeight();
+    var myViewSet = winH/3;
+    // console.log(innerBgOffset - (winH/3));
+    
 
     win.scroll(function(){
+        var it = $(this);
 
-        if(it.scrollTop() >= 700 ){
-            tabConImg.addClass('animate');
-        }
-        if(it.scrollTop() >= 300 ){
+        if(it.scrollTop() >= innerBgOffset-myViewSet ){
             mainBg.addClass('animate');
+            
         }
-        if(it.scrollTop() >= 1000 ){
-            storyP.addClass('animate');
-        }
-        
-        
+        if(it.scrollTop() >= mainBgOffset+(winH/5) ){
+            storyImg.addClass('story_animate');
+
+            setTimeout(function(){
+                if(it.scrollTop() >= mainBgOffset+(winH/4) ){
+                    storyP.addClass('story_animate');
+                }
+            },700);
+        } 
     });
-
-    
-
-   
-    
-
 
 
 
