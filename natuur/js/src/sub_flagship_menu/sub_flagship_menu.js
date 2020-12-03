e68a572e10dc574ebbd0e2adb04177f2834f8dee
wjdgotn001 <wjdgotn001@gmail.com>
2020-12-03 10:51:58 +0900

    variable_conversion

---
+++ natuur/js/src/sub_flagship_menu/sub_flagship_menu.js
@@ -0,0 +1,12 @@
+// sub_flagship_menu.js
+
+(function($){
+  $.ajax({
+    url : '../data/menu_list_data.json',
+    dataType : 'json',
+    context:document.body
+  }).done(function(data){
+    
+
+  });
+})(jQuery);
